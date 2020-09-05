45425bb4a7a646a6884875b38d7199fecebdbc3b
Svetlana Ryabova <ganyim@inbox.ru>
2020-09-05 22:47:23 +0500

    appv

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# ahj-dom
-[![Build status](https://ci.appveyor.com/api/projects/status/gdi6a9pxh5pg9i74?svg=true)](https://ci.appveyor.com/project/Milfagirl/ahj-dom)
+# ahj-events
+[![Build status](https://ci.appveyor.com/api/projects/status/9tsdoseqx0f94fac?svg=true)](https://ci.appveyor.com/project/Milfagirl/ahj-events)
--- src/js/goblin.js
+++ src/js/goblin.js
@@ -1,8 +1,8 @@
+/* eslint-disable no-param-reassign */
 export default function showGoblin(td, array, rand) {
   const img = '<img data-img="img" src="https://github.com/netology-code/ahj-homeworks/raw/master/dom/pic/goblin.png" alt="картинка">';
   array.forEach((el) => {
     el.innerHTML = '';
   });
   td.item(rand).innerHTML = img;
-} 
-
+}
